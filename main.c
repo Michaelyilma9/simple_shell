ab9248220ef32657eac81353927ba7f5c6e44971
Aleazar yilma <yilma4060@gmail.com>
2022-06-15 08:16:25 +0300

    First Fix

--- main.c
+++ main.c
@@ -1,39 +1,90 @@
 #include "main.h"
-/**
- * main - prints a command
- * @argc: argument  counter
- * @argv: array of arguments
- * Return: number of characters read
- */
-int main(int argc, char *argv[])
-{
-	store data;
-	char inputstr[MAXCOM], *command[MAXLIST];
-	int loop = 1;
-	(void) argc;
 
-	set_data(&data, argv[0]);
-	signal(SIGINT, ctrlchandler);
-	while (loop == 1)
+/**
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
+
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handel);
+	while (statue)
 	{
-		write(STDIN_FILENO, "$ ", 2);
-		if (storeinput(inputstr) == 0)
+		counter++;
+		if (isatty(STDIN_FILENO))
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
 		{
-			if (!remove_comment(inputstr))
-				continue;
-			_strcpy(inputstr, remove_comment(inputstr));
-			split_space(inputstr, command);
-			if (inputstr[0] != '\0')
-				cpathandexec(command, &data);
-			else
-				continue;
-			data.counter += 1;
+			continue;
+		}
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free_all(cmd, input);
+			continue;
 		}
 		else
-			break;
+		{
+			st = check_cmd(cmd, input, counter, argv);
+
+		}
+		free_all(cmd, input);
 	}
-	free_environ(&data);
-	if (data._return < 0)
-		return (255);
-	return (data._return);
+	return (statue);
+}
+/**
+ * check_builtin - check builtin
+ *
+ * @cmd:command to check
+ * Return: 0 Succes -1 Fail
+ */
+int check_builtin(char **cmd)
+{
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*cmd == NULL)
+	{
+		return (-1);
+	}
+
+	while ((fun + i)->command)
+	{
+		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
+}
+/**
+ * creat_envi - Creat Array of Enviroment Variable
+ * @envi: Array of Enviroment Variable
+ * Return: Void
+ */
+void creat_envi(char **envi)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+		envi[i] = _strdup(environ[i]);
+	envi[i] = NULL;
 }
--- main.h
+++ main.h
@@ -1,82 +1,112 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#define MAXCOM 1000
-#define BUFFER 1024
-#define MAXLIST 100
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <limits.h>
-#include <string.h>
+/**###### environ var ######*/
 
 extern char **environ;
-/**
- * struct store - contains some set data
- * @_environ: environment variables
- * @callmemaybe: argv[0]
- * @counter: line counter
- * @_return: return value
- */
-typedef struct store
-{
-	char **_environ;
-	char *callmemaybe;
-	int counter;
-	int _return;
-} store;
 
-void execArg(char **command, store *data);
-int _strcmp(char *s1, char *s2);
-void free_environ(store *data);
-int storeinput(char *str);
-char *_strcpy(char *dest, char *src);
-char *_itoa(int n);
-void split_space(char *str, char **command);
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
+/**###### LIBS USED ######*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
+
+
+
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
-char *_strcat(char *dest, char *src);
-int cpathandexec(char **command, store *data);
-int cknowncommand(char **command, store *data);
-void _puts(char *str);
 int _putchar(char c);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-void bring_line(char **lineptr, size_t *n, char *buf, size_t j);
-unsigned int is_delim(char c, char *delim);
-char *_strtok(char *str, char *delim);
 int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strdup(char *str);
-char *remove_comment(char *str);
-char *copy_info(char *name, char *value);
-void set_env(char *name, char *value, store *data);
-int _csetenv(char **command, store *data);
-void set_data(store *data, char *name);
-char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
-int _unsetenv(char **command, store *data);
-void rev_string(char *s);
-int cd_exec(char **command, store *data);
-void cd_to_home(char **command, store *data);
-void cd_previous(store *data);
-void cd_to(char **command, store *data);
-void cd_dot(char **command, store *data);
-void ctrlchandler(int signum);
-int exit_num(char **command, store *data);
-int _perror(char **command, int value, store *data);
-char *_strcat_cd(store *data, char **command, char *m, char *error, char *ver);
-char *rpath(char *src);
-char *error_get_cd(char **command, store *data);
-char *error_exit_shell(char **command, store *data);
-char *error_env(char **command, store *data);
-int _isdigit(const char *str);
-char *error_not_found(char **command, store *data);
-char *_getenv(const char *name, char **_environ);
-int cmp_env_name(const char *nenv, const char *name);
+
+/**###### MEMORIE  MANGMENT ####*/
+
+void free_env(char **env);
+void *fill_an_array(void *a, int el, unsigned int len);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_calloc(unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_all(char **input, char *line);
+
+/**###### INPUT Function ######*/
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+/** ###### Command parser and extractor ###*/
+
+int path_cmd(char **line);
+char *_getenv(char *name);
+char **parse_cmd(char *cmd);
+int handle_builtin(char **cmd, int er);
+void read_file(char *filename, char **argv);
+char *build(char *token, char *value);
+int check_builtin(char **cmd);
+void creat_envi(char **envi);
+int check_cmd(char **tokens, char *line, int count, char **argv);
+void treat_file(char *line, int counter, FILE *fd, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
+
+/** ####BUL FUNC #####*/
+
+void hashtag_handle(char *buff);
+int history(char *input);
+int history_dis(char **cmd, int er);
+int dis_env(char **cmd, int er);
+int change_dir(char **cmd, int er);
+int display_help(char **cmd, int er);
+int echo_bul(char **cmd, int er);
+void  exit_bul(char **cmd, char *input, char **argv, int c);
+int print_echo(char **cmd);
+
+/** ####error handle and Printer ####*/
+void print_number(unsigned int n);
+void print_number_in(int n);
+void print_error(char *line, int c, char **argv);
+void _prerror(char **argv, int c, char **cmd);
+
+
+/**
+ * struct bulltin - contain bultin to handle and function to excute
+ * @command:pointer to char
+ * @fun:fun to excute when bultin true
+ */
+
+typedef struct  bulltin
+{
+	char *command;
+	int (*fun)(char **line, int er);
+} bul_t;
 
 #endif
